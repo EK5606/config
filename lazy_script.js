10b18ce3c41f58636fbd2dea157f8ca54c75a80b
Your Name <you@example.com>
2025-11-18 15:45:16 +0800

    添加fakeip过滤规则

--- lazy_script.js
+++ lazy_script.js
@@ -185,7 +185,7 @@ const dnsConfig = {
   'respect-rules': true,
   'enhanced-mode': 'fake-ip',
   'fake-ip-range': '198.18.0.1/16',
-  'fake-ip-filter': ['*', '+.lan', '+.local', '+.market.xiaomi.com'],
+  'fake-ip-filter': ['*', '+.lan', '+.local', '+.market.xiaomi.com', 'ping.archlinux.org'],
   // 'default-nameserver': [...defaultDNS],
   nameserver: [...foreignDNS],
   'proxy-server-nameserver': [...foreignDNS],
