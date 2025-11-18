143b674c5ca66403b7d54e727aa13c6c59df51f9
Your Name <you@example.com>
2025-11-18 19:52:03 +0800

    添加no-resolve

--- lazy_script.js
+++ lazy_script.js
@@ -795,7 +795,7 @@ function main(config) {
     'GEOSITE,private,DIRECT',
     'GEOIP,private,DIRECT,no-resolve',
     'GEOSITE,cn,国内网站',
-    'GEOIP,cn,国内网站',
+    'GEOIP,cn,国内网站,no-resolve',
     'MATCH,其他外网'
   )
   config['proxy-groups'].push(
