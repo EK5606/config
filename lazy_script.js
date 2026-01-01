aaa141971af0e4062ee9e87daf38964c13e1cdaf
Your Name <you@example.com>
2026-01-01 19:52:16 +0800

    更改规则顺序

--- lazy_script.js
+++ lazy_script.js
@@ -961,11 +961,11 @@ function main(config) {
   rules.push(
     'RULE-SET,privateip,私有网络,no-resolve',
     ...(ruleOptions.media ? ['RULE-SET,steamip-cn,国内游戏,no-resolve'] : []),
+    'RULE-SET,cnip,国内网站',
     ...(ruleOptions.media ? ['RULE-SET,mediaip,国外媒体,no-resolve'] : []),
     ...(ruleOptions.games ? ['RULE-SET,gamesip,游戏服务,no-resolve'] : []),
     ...(ruleOptions.telegram ? ['RULE-SET,telegramip,Telegram,no-resolve'] : []),
     ...(ruleOptions.japan ? ['GEOIP,JP,日本网站,no-resolve'] : []),
-    'RULE-SET,cnip,国内网站',
     // 'NOT,((DST-PORT,80/443/8080/8888)),非标端口',
     'MATCH,漏网之鱼'
   )
