b24f63c60175cf61766de4d058c74d36c5193cae
EK5606 <136732996+EK5606@users.noreply.github.com>
2026-04-11 19:18:05 +0800

    Remove wildcard from fake-ip-filter

--- lazy_script.js
+++ lazy_script.js
@@ -175,7 +175,6 @@ const dnsConfig = {
   'fake-ip-range6': 'fc00::1/64',
   'fake-ip-filter-mode': 'blacklist',
   'fake-ip-filter': [
-    '*',
     'rule-set:fakeip-filter,cn,private,trackerslist',
     'geosite:category-ntp,connectivity-check', 
   ],
